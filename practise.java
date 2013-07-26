25594b8cfdaf4cf843889504bf316a11babe9eef
ericdeng11 <ericdeng11@gmail.com>
2013-07-26 13:55:53 -0400

    add DP solution to decode ways in leetcode

--- practise.java
+++ practise.java
@@ -385,6 +385,32 @@ public static int queen(int[] chess, int level, int n ,int count){
     		}
     	}
     }
+    //leetcode: DP to solve decode ways
+    public int numDecodings(String s) {
+    if (s.length() == 0) {
+        return 0;
+    }
+
+    int[] count = new int[s.length() + 1];
+    count[0] = 1;
+    for (int i = 0, j = 1; i < s.length(); i++, j++) {
+        if (s.charAt(i) == '0') {
+            if (i > 0 && (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2')) {
+                count[j] = count[j - 2];
+            } else {
+                return 0;
+            }
+        } else if (i > 0
+                && (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s
+                        .charAt(i) <= '6'))) {
+            count[j] = count[j - 2] + count[j - 1];
+        } else {
+            count[j] = count[j - 1];
+        }
+    }
+
+    return count[s.length()];
+}
 	
 	
 
