0ff250b768e1b430ae663231ade38c0816ca97e4
QiangDeng <ericdeng11@gmail.com>
2013-08-23 10:48:50 -0400

    bs

--- binarySearch.java
+++ binarySearch.java
@@ -2,14 +2,18 @@ import java.util.*;
 public class binarySearch{
 
 public static void main(String[] args){
-  int[] arr = new int[5];
+  int[] arr = new int[6];
   arr[0] = 1;
   arr[1] = 7;
   arr[2] = 10;
   arr[3] = 2323;
   arr[4] = 103324;
-
+  arr[5] = 1000000;
   System.out.println(bs(1, arr, 0, arr.length - 1));
+  List<Integer> l = new ArrayList<Integer>();
+  l.add(1);
+  l.add(2);
+  System.out.println(l.get(0));
 }
 
 public static int bs(int key, int[] arr, int left, int right){
